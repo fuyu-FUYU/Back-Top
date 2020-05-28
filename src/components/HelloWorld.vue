2275fc1c2a33ed5747ca713b205bf72abd29a31f
fuyu <fuyu@123.com>
2020-05-28 17:29:03 +0800

    add

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+ <div id="app" style="height:2000px">
+        <Top></Top>
+  </div>
+</template>
+
+<script>
+import Top from "../views/Top"
+export default {
+name:"",
+data() {
+return {
+
+}
+},
+components:{
+    Top
+},
+//生命周期 - 创建完成（访问当前this实例）
+created() {
+
+},
+//生命周期 - 挂载完成（访问DOM元素）
+mounted() {
+
+}
+}
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+
+</style>
